4fc3d569875f4555153961faaa5adf7509b64ffe
DongJae Kim <ehdwo1227@naver.com>
2025-11-02 03:20:35 +0900

    feat(Application): 클래스 통합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Input input = new Input();
+        int lottoPurchaseAmount = Integer.parseInt(input.getLottoPurchaseAmount());
+        String lottoNumber = input.getLottoNumber();
+        int bonusNumber = Integer.parseInt(input.getBonusNumber());
+
+        WinningLotto winningLotto = new WinningLotto(lottoNumber);
+        List<Integer> winningLottoNum = winningLotto.getWinningLotto();
+
+        LottoGenerator lottoGenerator = new LottoGenerator(lottoPurchaseAmount);
+        List<Lotto> generatedLottos = lottoGenerator.getGeneratedLotto();
+
+
+
+        LottoResult lottoResult = new LottoResult(generatedLottos, winningLottoNum, bonusNumber);
+        System.out.println(lottoResult.calculateTotalPrize());
     }
 }
