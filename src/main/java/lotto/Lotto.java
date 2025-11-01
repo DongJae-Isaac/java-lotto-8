46bf6c9f6acf7aa909d5485aab4d92e406956536
DongJae Kim <ehdwo1227@naver.com>
2025-11-02 03:17:38 +0900

    feat(Lotto): 로또 객체 관리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,22 @@
-package lotto;
+    package lotto;
 
-import java.util.List;
+    import java.util.List;
 
-public class Lotto {
-    private final List<Integer> numbers;
+    public class Lotto {
+        private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+        public Lotto(List<Integer> numbers) {
+            validate(numbers);
+            this.numbers = numbers;
+        }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        public List<Integer> getNumbers() {
+            return numbers;
+        }
+
+        private void validate(List<Integer> numbers) {
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            }
         }
     }
-
-    // TODO: 추가 기능 구현
-}
