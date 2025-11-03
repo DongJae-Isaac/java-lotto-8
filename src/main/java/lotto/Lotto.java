76614321d310e8607e28176a99610c836b873044
DongJae Kim <ehdwo1227@naver.com>
2025-11-03 16:38:36 +0900

    refactor(Lotto): 로또 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,19 @@
     package lotto;
 
+    import java.util.HashSet;
     import java.util.List;
+    import java.util.Set;
 
     public class Lotto {
+        private static final int LOTTO_NUMBER_SIZE = 6;
+        private static final int MIN_LOTTO_NUMBER = 1;
+        private static final int MAX_LOTTO_NUMBER = 45;
         private final List<Integer> numbers;
 
         public Lotto(List<Integer> numbers) {
-            validate(numbers);
+            validateSize(numbers);
+            validateRange(numbers);
+            validateDuplicate(numbers);
             this.numbers = numbers;
         }
 
@@ -14,9 +21,24 @@
             return numbers;
         }
 
-        private void validate(List<Integer> numbers) {
-            if (numbers.size() != 6) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        private void validateSize(List<Integer> numbers) {
+            if (numbers.size() != LOTTO_NUMBER_SIZE) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_SIZE.getMessage());
+            }
+        }
+
+        private void validateRange(List<Integer> numbers) {
+            for (Integer number : numbers) {
+                if(number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+                    throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE.getMessage());
+                }
+            }
+        }
+
+        private void validateDuplicate(List<Integer> numbers) {
+            Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+            if(uniqueNumbers.size() != LOTTO_NUMBER_SIZE) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_DUPLICATE.getMessage());
             }
         }
     }
