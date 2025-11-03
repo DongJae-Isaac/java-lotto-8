b86388b4f2b67f5eaff7913401c3a932487fab77
DongJae Kim <ehdwo1227@naver.com>
2025-11-03 17:22:45 +0900

    refactor(Input): 출력 문자열 수정

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     public int getLottoPurchaseAmount(){
-        System.out.println("구입 금액을 입력해주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
         String input =  Console.readLine();
 
         try {
