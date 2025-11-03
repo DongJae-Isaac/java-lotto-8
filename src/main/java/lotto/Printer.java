08352d41dff8ebb91c2b49a3894b1aeb2e0814b9
DongJae Kim <ehdwo1227@naver.com>
2025-11-03 17:23:58 +0900

    refactor(Printer): 로또 결과 출력순서 수정

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -12,18 +12,22 @@ public class Printer {
     }
 
     public void printGeneratedLotto(List<Lotto> generatedLottos) {
-        System.out.println(lottoPurchaseAmount + "개를 구매했습니다.");
+        int LOTTO_PRICE = 1000;
+        System.out.println(lottoPurchaseAmount/ LOTTO_PRICE + "개를 구매했습니다.");
         for (Lotto generatedLotto : generatedLottos) {
-            List<Integer> generatedLottoNumbers = generatedLotto.getNumbers();
-            System.out.println(generatedLottoNumbers);
+            System.out.println(generatedLotto.getSortedNumber());
         }
     }
 
     public void printLottoResult(LottoResult lottoResult) {
         Map<Rank, Integer> lottoRank = lottoResult.calculateLottoRank();
-        for (Entry<Rank, Integer> rankEntry : lottoRank.entrySet()) {
-            Rank rankKey = rankEntry.getKey();
-            System.out.println(rankKey.getDescription() + "-" + rankEntry.getValue());
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        for (Rank rank : Rank.values()) {
+            int count = lottoRank.getOrDefault(rank, 0);
+            System.out.println(rank.getDescription() + " - " + count + "개");
         }
 
         System.out.printf("총 수익률은 %.1f%%입니다.%n", lottoResult.calculateTotalYield(lottoPurchaseAmount));
