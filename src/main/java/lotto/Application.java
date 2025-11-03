a46155f7126d75978c9b3470e952f3c2dfe16e1a
DongJae Kim <ehdwo1227@naver.com>
2025-11-03 10:39:12 +0900

    feat(Application): 로또 프로그램 통합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,24 +3,30 @@ package lotto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Application {
     public static void main(String[] args) {
 
         Input input = new Input();
         int lottoPurchaseAmount = Integer.parseInt(input.getLottoPurchaseAmount());
+
+        Printer printer = new Printer(lottoPurchaseAmount);
+        // 로또 생성
+        LottoGenerator lottoGenerator = new LottoGenerator(lottoPurchaseAmount);
+        List<Lotto> generatedLottos = lottoGenerator.getGeneratedLotto();
+        // 생성한 로또 출력
+        printer.printGeneratedLotto(generatedLottos);
+
         String lottoNumber = input.getLottoNumber();
         int bonusNumber = Integer.parseInt(input.getBonusNumber());
 
         WinningLotto winningLotto = new WinningLotto(lottoNumber);
         List<Integer> winningLottoNum = winningLotto.getWinningLotto();
 
-        LottoGenerator lottoGenerator = new LottoGenerator(lottoPurchaseAmount);
-        List<Lotto> generatedLottos = lottoGenerator.getGeneratedLotto();
-
-
-
+        // 로또 결과 출력
         LottoResult lottoResult = new LottoResult(generatedLottos, winningLottoNum, bonusNumber);
-        System.out.println(lottoResult.calculateTotalPrize());
+        printer.printLottoResult(lottoResult);
     }
 }
